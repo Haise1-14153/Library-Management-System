04cf51c72736521dc776d8d7c78ac09c46098f81
Mr. Hacker Haise <jarameldanriel777@gmail.com>
2024-05-24 02:24:21 +0800

    Add files via upload

--- adminProject/src/CREATEDATABASE.java
+++ adminProject/src/CREATEDATABASE.java
@@ -41,13 +41,39 @@ public class CREATEDATABASE {
             prestmt = conn.prepareStatement(query);
             prestmt.executeUpdate();
             //create useraccountinfo
-            query = "create table staffaccountinfo (username varchar(100),pass varchar(100),firstname varchar(100),lastname varchar(100),"
+            query = "create table useraccountinfo (username varchar(100),pass varchar(100),firstname varchar(100),lastname varchar(100),"
                     + "birthmonth varchar(10),birthday int,birthyear int,gender varchar(10),student_ID int,cash_balance double,days_banned int,datecreated varchar(50))";
             prestmt = conn.prepareStatement(query);
             prestmt.executeUpdate();
-            //create useraccountinfo
-            query = "create table staffaccountinfo (username varchar(100),pass varchar(100),firstname varchar(100),lastname varchar(100),"
-                    + "birthmonth varchar(10),birthday int,birthyear int,gender varchar(10),student_ID int,cash_balance double,days_banned int,datecreated varchar(50))";
+            //create bookinformation
+            query = "create table bookinformation (BookID int,Book_Name varchar(300),Author varchar(300),Overview varchar(300),"
+                    + "Language_ varchar(30),Category_ varchar(100),Condition_ varchar(10)),Book_Number int,Score double,Overall_Score double,available tinyint";
+            prestmt = conn.prepareStatement(query);
+            prestmt.executeUpdate();
+            //create borrowedbooks
+            query = "create table borrowedbooks (StudentID int,Student_Name varchar(100),BookID int,Book_Name varchar(300)"
+                    + ",Date_Borrowed varchar(30),Date_Returned varchar(30),Due_Date varchar(30),Fine_per_day double,payable double,Condition_Borrowed varchar(20),Condition_Returned varchar(20))";
+            prestmt = conn.prepareStatement(query);
+            prestmt.executeUpdate();
+            //create borrowhistory
+            query = "create table borrowedbooks (StudentID int,Student_Name varchar(100),BookID int,Book_Name varchar(300)"
+                    + ",Date_Borrowed varchar(30),Date_Returned varchar(30),Due_Date varchar(30),Fine_per_day double,payable double,Condition_Borrowed varchar(20),Condition_Returned varchar(20))";
+            prestmt = conn.prepareStatement(query);
+            prestmt.executeUpdate();
+            //create codeauthentication
+            query = "create table codeauthentication (Email varchar(100),Code_ int)";
+            prestmt = conn.prepareStatement(query);
+            prestmt.executeUpdate();
+            //create notification
+            query = "create table notification (email varchar(300))";
+            prestmt = conn.prepareStatement(query);
+            prestmt.executeUpdate();
+            //create overduefees
+            query = "create table overduefees (ID int,Borrow_Period int,Fine_per_day double)";
+            prestmt = conn.prepareStatement(query);
+            prestmt.executeUpdate();
+            //create reservation
+            query = "create table reservation (student_id int,student_name varchar(100),book_id int,book_name varchar(300),book_number int,date_reserved varchar(50))";
             prestmt = conn.prepareStatement(query);
             prestmt.executeUpdate();
         } catch (ClassNotFoundException ex) {
